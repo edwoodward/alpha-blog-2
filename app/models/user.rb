611c2041dc3dd5c2265f1d4fbef26054b9d1c482
edwoodward <ecw1@rice.edu>
2020-04-17 16:14:41 -0500

    Added one to many relationship with User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
-class User < ApplicationReacord
+class User < ApplicationRecord
+  has_many :articles
   validates :username, presence: true, uniqueness: {case_sensiticwe: false}, length: {minimum: 3, maximum:25}
-  VALID_EMAIL_REGEX = /\A[^@\s]+@[^@\s]+\z/
-  validates :email, presence: true, uniqueness: {case_sensiticwe: false}, length: {maximum:105}, format: {with: VALID_EMAIL_FORMAT}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: {case_sensiticwe: false}, length: {maximum:105}, format: {with: VALID_EMAIL_REGEX}
 
 end
