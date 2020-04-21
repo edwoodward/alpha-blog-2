2dee95f9f9f1140119ee2b5448a80f4a6ccb5e99
edwoodward <ecw1@rice.edu>
2020-04-20 20:52:47 -0500

    Added useer profile page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+
+  def image_for(user)
+    image_tag 'https://openstax.org/images/rover-by-openstax/rover-angled-corner.svg', alt: user.username, class: "rounded shadow mx-auto d-block"
+  end
 end
