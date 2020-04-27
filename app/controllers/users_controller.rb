5e88c46902cd25a397ff0098fa9cc5ef4a14c4bb
edwoodward <ecw1@rice.edu>
2020-04-26 19:34:33 -0500

    added admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,17 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def show
     @articles = @user.articles = Article.paginate(page: params[:page], per_page: 3)
+  end
+
+  def destroy
+    @user = User.find(user_params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by the user have been deleted."
+    redirect_to users_path
   end
 
   def index
@@ -50,8 +58,15 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if helpers.current_user != @user
+    if helpers.current_user != @user && !helpers.current_user.admin?
       flash[:danger] = "You can only edit your own profile"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if helpers.logged_in? && !helpers.current_user.admin?
+      flash[:danger] = "You must be an admin to perform that action"
       redirect_to root_path
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   before_save {email = self.email.downcase }
-  has_many :articles
+  has_many :articles, dependent: :destroy
   validates :username, presence: true, uniqueness: {case_sensiticwe: false}, length: {minimum: 3, maximum:25}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: {case_sensiticwe: false}, length: {maximum:105}, format: {with: VALID_EMAIL_REGEX}
