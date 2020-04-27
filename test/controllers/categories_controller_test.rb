4aababd83b24b97c69a692946270a22e1784a83f
edwoodward <ecw1@rice.edu>
2020-04-26 20:30:50 -0500

    Start of adding categories and tests

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class CategoriesControllerTest < ActionDispatch::IntegrationTest
+
+  test "should get categories index" do
+    get categories_path
+    assert_response :success
+    
+  end
+
+  test "should get new" do
+    get new_category_path
+    assert_response :success
+    
+  end
+
+  test "should get show" do
+    get categoty_path
+    assert_response :success
+    
+  end
+
+end
